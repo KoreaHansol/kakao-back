5284f6cfa868341446016e781ca94259a89f449b
KoreaHansol <hskimaa@hanmail.net>
2022-02-02 22:51:10 +0900

    commit

--- index.js
+++ index.js
@@ -3,16 +3,26 @@ const app = express()
 const cors = require( 'cors' )
 const mysql = require( 'mysql' )
 const port = 8080
+const moment = require( 'moment' )
+const http = require( "http" )
+const socketIo = require("socket.io");
+const server = http.createServer( app )
+
 
 const bodyParser = require( 'body-parser' )
+const { allowedNodeEnvironmentFlags } = require('process')
 app.use( bodyParser.urlencoded( { extended: true } ) )
 app.use( bodyParser.json() )
 
-const conn = mysql.createConnection( {
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
+let conn = mysql.createConnection( {
   host: 'localhost',
   user: 'root',
   password: '1234',
-  database: 'kakao'
 } )
 
 conn.connect( ( err ) => {
@@ -23,18 +33,85 @@ conn.connect( ( err ) => {
     if( err ) throw err
   } )
 
+  conn = mysql.createConnection( {
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'kakao'
+  } )
+
   const sql = `CREATE TABLE IF NOT EXISTS user (
-    userId INT NOT NULL AUTO_INCREMENT,
-    userName VARCHAR(45) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(45) NOT NULL,
     password VARCHAR(45) NOT NULL,
-    PRIMARY KEY (userId),
-    UNIQUE INDEX userId_UNIQUE (userId ASC),
-    UNIQUE INDEX userName_UNIQUE (userName ASC))`
+    name VARCHAR(45) NOT NULL,
+    create_date VARCHAR(45) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE INDEX email_UNIQUE (email ASC),
+    UNIQUE INDEX id_UNIQUE (id ASC))`
 
     conn.query( sql, ( err, result ) => {
       if( err ) throw err
     } )
+
+    const room = `CREATE TABLE IF NOT EXISTS room (
+      id INT NOT NULL AUTO_INCREMENT,
+      title VARCHAR(45) NOT NULL,
+      owner VARCHAR(45),
+      create_date VARCHAR(45) NOT NULL,
+      PRIMARY KEY (id),
+      UNIQUE INDEX id_UNIQUE (id ASC))`
+
+    conn.query( room, ( err, result ) => {
+      if( err ) throw err
+    } )
+    
+    const chat = `CREATE TABLE IF NOT EXISTS chat (
+      id INT NOT NULL AUTO_INCREMENT,
+      image VARCHAR(45),
+      user VARCHAR(45) NOT NULL,
+      chat VARCHAR(45) NOT NULL,
+      create_date VARCHAR(45) NOT NULL,
+      PRIMARY KEY (id),
+      UNIQUE INDEX id_UNIQUE (id ASC))`
+
+    conn.query( chat, ( err, result ) => {
+      if( err ) throw err
+    } )
+
+    const sql2 = `alter table user convert to character set utf8`
+    conn.query( sql2, ( err, result ) => {
+      if( err ) throw err
+    } )
 } )
+
+const io = socketIo( server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+} )
+
+let roomName
+
+io.on( "connection", ( socket ) => {
+  console.log( "New client connected" )
+  socket.on( 'init', (res) => {
+    console.log( res )
+  } )
+
+  socket.on( 'join', ( data ) => {
+    console.log( 'join', data )
+    socket.join( data.roomName )
+    roomName = data.roomName
+    
+  } )
+  socket.on( "disconnect", () => {
+    console.log("Client disconnected")
+  } )
+} )
+
+
 
 app.use(cors({
   origin: '*'
@@ -45,31 +122,73 @@ app.get( '/', ( req, res ) => {
   res.send( 'connect success' )
 } )
 
-app.post( '/registeruser', async ( req, res ) => {
-
-  console.log( req.body )
-  res.send( {
-    code: 200,
-    payload: {
-      asd: 1
-    }
-  } )
-
-  let event = {
-    // header : req.headers,
-    query : req.query,
-    asd: req.data,
-    body : req.body,
-    query2 : res.query,
-    body2 : res.body,
-    query3 : res.params,
-    body3 : res.params,
-    query4 : req.params,
-    body5 : req.params,
-  }
+app.post( '/registeruser', ( req, res, next ) => {
+  const { name, password, email } = req.body
+  const now = moment().valueOf()
+  const sql = `INSERT INTO user( name, password, email, create_date )
+  VALUES ( '${name}', '${password}', '${email}', '${now}' )`
+    conn.query( sql, ( err, result ) => {
+      if( err ) {
+        res.send( {
+          code: 404
+        } )
+        next( err )
+      } else {
+        console.log( 'asd' )
+        res.send( {
+          code: 200,
+          payload: {
+            result
+          }
+        } )
+      }
+    } )
 } )
 
-const server = app.listen( port, () => {
+app.post( '/validateuser', ( req, res, next ) => {
+  const { email, password } = req.body
+  console.log( email, password )
+  const sql = `select * from user where email = '${email}' and password = '${password}'`
+  conn.query( sql, ( err, result ) => {
+    console.log( result )
+    if( err ) {
+      res.send( {
+        code: 404
+      } )
+      next( err )
+    } else {
+      res.send( {
+        code: 200,
+        payload: {
+          result
+        }
+      } )
+    }
+  } )
+} )
+
+app.get( '/getuserlist', ( req, res, next ) => {
+  const sql = `select * from user`
+  conn.query( sql, ( err, result ) => {
+    if( err ) {
+      res.send( {
+        code: 404
+      } )
+      next( err )
+    } else {
+      res.send( {
+        code: 200,
+        payload: {
+          result
+        }
+      } )
+    }
+  } )
+} )
+
+
+
+server.listen( port, () => {
   const host = server.address().address
   const port = server.address().port
 
